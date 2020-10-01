788b846f350a44c9716840712e8cc5df371eb69f
SreejithNS <nssreejith229@gmail.com>
2020-10-01 14:46:20 +0530

    Static content now from client/build

--- server/app.js
+++ server/app.js
@@ -34,7 +34,7 @@ if(process.env.NODE_ENV !== "test") {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 //To allow cross-origin requests
 app.use(cors());
