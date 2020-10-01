73f11e4a1018f5c67eb95f08f5fa867fed532d18
SreejithNS <nssreejith229@gmail.com>
2020-10-01 13:58:14 +0530

    Added js docs

--- server/helpers/apiResponse.js
+++ server/helpers/apiResponse.js
@@ -1,3 +1,8 @@
+/**
+ * 
+ * @param {*} res 
+ * @param {*} msg 
+ */
 exports.successResponse = function (res, msg) {
 	var data = {
 		status: 1,
@@ -5,7 +10,12 @@ exports.successResponse = function (res, msg) {
 	};
 	return res.status(200).json(data);
 };
-
+/**
+ * 
+ * @param {*} res 
+ * @param {*} msg 
+ * @param {*} data 
+ */
 exports.successResponseWithData = function (res, msg, data) {
 	var resData = {
 		status: 1,
@@ -14,7 +24,11 @@ exports.successResponseWithData = function (res, msg, data) {
 	};
 	return res.status(200).json(resData);
 };
-
+/**
+ * 
+ * @param {*} res 
+ * @param {*} msg 
+ */
 exports.ErrorResponse = function (res, msg) {
 	var data = {
 		status: 0,
@@ -22,7 +36,11 @@ exports.ErrorResponse = function (res, msg) {
 	};
 	return res.status(500).json(data);
 };
-
+/**
+ * 
+ * @param {*} res 
+ * @param {*} msg 
+ */
 exports.notFoundResponse = function (res, msg) {
 	var data = {
 		status: 0,
@@ -31,6 +49,12 @@ exports.notFoundResponse = function (res, msg) {
 	return res.status(404).json(data);
 };
 
+/**
+ * 
+ * @param {*} res 
+ * @param {*} msg 
+ * @param {*} data 
+ */
 exports.validationErrorWithData = function (res, msg, data) {
 	var resData = {
 		status: 0,
@@ -40,6 +64,11 @@ exports.validationErrorWithData = function (res, msg, data) {
 	return res.status(400).json(resData);
 };
 
+/**
+ * 
+ * @param {*} res 
+ * @param {*} msg 
+ */
 exports.unauthorizedResponse = function (res, msg) {
 	var data = {
 		status: 0,
