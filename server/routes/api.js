c90133c4b5a3f4327e7d39e4ab630fba5707145f
SreejithNS <nssreejith229@gmail.com>
2020-10-06 00:24:38 +0530

    Added /customer to routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var authRouter = require("./auth");
 var bookRouter = require("./book");
+var customerRouter = require("./customer");
 
 var app = express();
 
 app.use("/auth/", authRouter);
 app.use("/book/", bookRouter);
+app.use("/customer/", customerRouter); // All Customer Data Related Functions
 
 module.exports = app;
