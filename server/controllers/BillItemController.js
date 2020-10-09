7e2a828f9314362f30594239922a025f35a57c7d
SreejithNS <nssreejith229@gmail.com>
2020-10-09 18:09:27 +0530

    Todo for creating bill item

--- server/controllers/BillItemController.js
+++ server/controllers/BillItemController.js
@@ -43,17 +43,17 @@ exports.delete = [
     }
 ];
 
-
-module.exports = [
+//TODO: Create a product from code,name,rate,mrp.  
+exports.create = [
     auth,
     body("code").escape().isLength().trim().isAlphanumeric().custom((value)=>{
         return Product.findOne({code:value}).then((doc)=>{
             if(doc) return Promise.reject("Item with this code already exists.");
         });
     }),
-    body("name").optional().escape().isLength().trim().isAlphanumeric(),
-    body("rate").optional().escape().trim().isNumeric(),
-    body("mrp").optional().escape().trim().isNumeric(),
+    body("name").escape().isLength().trim().isAlphanumeric(),
+    body("rate").escape().trim().isNumeric(),
+    body("mrp").escape().trim().isNumeric(),
     body("weight").optional().escape().trim().isNumeric(),
     body("weightUnit").optional().escape().trim().custom((value)=>{
         return /\b(?:administrator|editor|contributor|user)\b/.exec(value)?true:Promise.reject("Please enter weight");
