b71b608bf2a9882ed4968dbab6d3f1ccbdabbacf
Kinshuk Dua <kinshukduaexam@gmail.com>
2020-10-09 18:25:39 +0530

    Update exports.create in BillItemController

--- server/controllers/BillItemController.js
+++ server/controllers/BillItemController.js
@@ -57,5 +57,35 @@ exports.create = [
     body("weight").optional().escape().trim().isNumeric(),
     body("weightUnit").optional().escape().trim().custom((value)=>{
         return /\b(?:administrator|editor|contributor|user)\b/.exec(value)?true:Promise.reject("Please enter weight");
+
     }),
+    sanitizeBody("*").escape(),
+    (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return apiResponse.validationErrorWithData(res, "Validation Error.", errors.array());
+            } else {
+                let newProduct= new Product(
+                    _.pickBy({
+                        code: req.body.code,
+                        name: req.body.name,
+                        weight: req.body.weight,
+                        weightUnit: req.body.weight,
+                        rate: req.body.rate,
+                        mrp: req.body.mrp,
+                    }, _.identity));
+
+                //Save Customer.
+                newCustomer.save(function (err) {
+                    if (err) { return apiResponse.ErrorResponse(res, err); }
+                    let customerData = new CustomerData(newCustomer);
+                    return apiResponse.successResponseWithData(res, "Product add Success.", customerData);
+                });
+            }
+        } catch (err) {
+            //throw error in json response with status 500. 
+            return apiResponse.ErrorResponse(res, err);
+        }
+    }
 ];
