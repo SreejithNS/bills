ada272a157536a099915f91013f7759f114d6f62
SreejithNS <nssreejith229@gmail.com>
2020-10-10 20:21:56 +0530

    Added bill api to route

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,6 +3,7 @@ var authRouter = require("./auth");
 var bookRouter = require("./book");
 var customerRouter = require("./customer");
 var productRouter = require("./product");
+var billRouter = require("./bill");
 
 var app = express();
 
@@ -10,5 +11,6 @@ app.use("/auth/", authRouter);
 app.use("/book/", bookRouter);
 app.use("/customer/", customerRouter); // All Customer Data Related Functions
 app.use("/product/", productRouter);
+app.use("/bill/",billRouter);
 
 module.exports = app;
