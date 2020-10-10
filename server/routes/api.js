2248e5a3e9cdd416c4e503deee2c78fdd1808dec
SreejithNS <nssreejith229@gmail.com>
2020-10-11 01:03:54 +0530

    Minor fixes

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var authRouter = require("./auth");
-var bookRouter = require("./book");
 var customerRouter = require("./customer");
 var productRouter = require("./product");
 var billRouter = require("./bill");
@@ -8,7 +7,6 @@ var billRouter = require("./bill");
 var app = express();
 
 app.use("/auth/", authRouter);
-app.use("/book/", bookRouter);
 app.use("/customer/", customerRouter); // All Customer Data Related Functions
 app.use("/product/", productRouter);
 app.use("/bill/",billRouter);
