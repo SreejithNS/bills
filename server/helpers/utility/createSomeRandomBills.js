19f9413b98f06c9eb79d5ce3224b537b0a408491
SreejithNS <nssreejith229@gmail.com>
2020-10-10 21:26:09 +0530

    Random Bill population utility

---
+++ server/helpers/utility/createSomeRandomBills.js
@@ -0,0 +1,64 @@
+
+require("dotenv").config();
+
+// DB connection
+var MONGODB_URL = process.env.MONGODB_URL;
+var mongoose = require("mongoose");
+const BillModel = require("../../models/BillModel");
+const CustomerModel = require("../../models/CustomerModel");
+const ProductModel = require("../../models/ProductModel");
+
+mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        //don't show the log when it is test
+        if (process.env.NODE_ENV !== "test") {
+            console.log("Connected to %s", MONGODB_URL);
+            generateRandomBills(5,3,10)
+                .then((queue)=>{
+                    console.log(`Bills Generator:\nCount:${queue.length}`);
+                    return BillModel.insertMany(queue);
+                },console.error)
+                .then((docs)=>{
+                    console.log(`Inserted ${docs.length} new Bill`);
+                    process.exit();
+                },console.error);
+        }
+    })
+    .catch(err => {
+        console.error("App starting error:", err.message);
+        process.exit(1);
+    });
+
+async function generateRandomBills(billCount = 5, productsCount = 3,maxQuantity=10) {
+    const products = await ProductModel.find({}).lean().exec();
+    const customers = await CustomerModel.find({},"_id").lean().exec();
+
+    const getRandomItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+    const getRandomCount = (max) => Math.floor(Math.random() * max) + 1;
+
+    const insertQueue = [];
+
+    for (var billIteration = 0; billIteration < billCount; billIteration++) {
+        const customerId = getRandomItem(customers)._id;
+        const request = {
+            customer: customerId,
+            items:[],
+            discountAmount:0
+        };
+
+        for (var productIteration = 0; productIteration < getRandomCount(productsCount); productIteration++){
+            const product = getRandomItem(products);
+            product.quantity = getRandomCount(maxQuantity);
+
+            request.items.push(product);
+        }
+        
+        const bill = new BillModel(request);
+        bill.itemsTotalAmount = bill.calculateItemsTotalAmount();
+        bill.billAmount = bill.calculateBillAmount();
+
+        insertQueue.push(bill);
+    }
+
+    return insertQueue;
+}
