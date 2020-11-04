78f2eac656269bc57846d87746fb66046c88a12f
SreejithNS <nssreejith229@gmail.com>
2020-11-04 14:44:22 +0530

    Fixed wrongly handled static routing with conditions

--- server/app.js
+++ server/app.js
@@ -1,25 +1,28 @@
 var express = require("express");
-var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 require("dotenv").config();
 var indexRouter = require("./routes/index");
 var apiRouter = require("./routes/api");
 var apiResponse = require("./helpers/apiResponse");
+var path = require("path");
 var cors = require("cors");
 
 // DB connection
 var MONGODB_URL = process.env.MONGODB_URL;
 var mongoose = require("mongoose");
-mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-	//don't show the log when it is test
-	if (process.env.NODE_ENV !== "test") {
-		console.log("Connected to %s", MONGODB_URL);
-		console.log("App is running ... \n");
-		console.log("Press CTRL + C to stop the process. \n");
-	}
-})
-	.catch(err => {
+const authenticate = require("./middlewares/jwt");
+mongoose
+	.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		//don't show the log when it is test
+		if (process.env.NODE_ENV !== "test") {
+			console.log("Connected to %s", MONGODB_URL);
+			console.log("App is running ... \n");
+			console.log("Press CTRL + C to stop the process. \n");
+		}
+	})
+	.catch((err) => {
 		console.error("App starting error:", err.message);
 		process.exit(1);
 	});
@@ -34,13 +37,23 @@ if (process.env.NODE_ENV !== "test") {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "../client/build")));
+// app.use(express.static(path.join(__dirname, "../client/build")));
+// app.use(express.static(path.join(__dirname, "./public")));
 
 //To allow cross-origin requests
 app.use(cors());
 
 //Route Prefixes
-app.use("/", indexRouter);
+app.all("/", indexRouter);
+app.use(
+	"/login",
+	authenticate,
+	function (req, res, next) {
+		if (req.user) res.redirect("/");
+		else next();
+	},
+	express.static(path.join(__dirname, "./public"))
+); //loginRouter);
 app.use("/api/", apiRouter);
 
 // throw 404 if URL not found
@@ -56,9 +69,9 @@ app.all("*", function (req, res) {
 
 app.use(function (err, req, res, next) {
 	if (err.name === "UnauthorizedError") {
-		apiResponse.unauthorizedResponse(res, err.message);
-	} else
-		next(err);
+		if (req.path === "/") res.redirect("/login");
+		else apiResponse.unauthorizedResponse(res, err.message);
+	} else next(err);
 });
 
 module.exports = app;
