60ed076ea50b03c7b218b9a2ec440f29f89c9b7c
SreejithNS <nssreejith229@gmail.com>
2020-11-04 14:42:51 +0530

    Added expections to login and updatedGetToken

--- server/middlewares/jwt.js
+++ server/middlewares/jwt.js
@@ -2,7 +2,22 @@ const jwt = require("express-jwt");
 const secret = process.env.JWT_SECRET;
 
 const authenticate = jwt({
-	secret: secret
-});
+	secret: secret,
+	getToken: (req) => {
+		if (req.cookies.token) return req.cookies.token;
+		//Get from cookie token
+		else if (req.body.token) return req.body.token;
+		// or from {token:"xyz"}
+		else if (
+			req.headers.authorization &&
+			req.headers.authorization.split(" ")[0] === "Bearer"
+		) {
+			return req.headers.authorization.split(" ")[1]; // or from header {Authorization : Bearer xyz}
+		} else if (req.query && req.query.token) {
+			return req.query.token; // or from query { token=xyz }
+		}
+		return null;
+	},
+}).unless({ path: [/login\/.*/gm, "/login"] });
 
 module.exports = authenticate;
