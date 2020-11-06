81a7cb0b61676c59c2b533ff244f8bb99168855e
SreejithNS <nssreejith229@gmail.com>
2020-11-06 20:52:05 +0530

    Added redux forms reducer

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form";
 import customerReducer from "./customer.reducer";
 
 const rootReducer = combineReducers({
 	customer: customerReducer,
+	form: formReducer,
 });
 
 export default rootReducer;
