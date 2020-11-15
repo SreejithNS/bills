1abb0a176095d6d272c8a8b8e95872ad136bd59a
SreejithNS <nssreejith229@gmail.com>
2020-11-16 00:46:35 +0530

    Added toastify toast container

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,13 @@ import {
   //Link
 } from "react-router-dom";
 import Routes from './routes';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <main>
+      <ToastContainer />
       <Router>
         <Routes />
       </Router>
