5afb8106106a33b7d0a57be09e69311343ad1f4e
SreejithNS <nssreejith229@gmail.com>
2020-11-23 18:11:22 +0530

    Added routes for product queries

--- server/routes/product.js
+++ server/routes/product.js
@@ -3,10 +3,12 @@ const BillItemController = require("../controllers/BillItemController");
 
 var router = express.Router();
 
+router.get("/query", BillItemController.query);
+router.get("/suggestions/:code", BillItemController.getSuggestions);
+router.get("/availability/:code", BillItemController.productAvailability);
 router.get("/:id", BillItemController.get);
-router.get("/suggestions/:code",BillItemController.getSuggestions);
 router.post("/", BillItemController.create);
-router.put("/:id",BillItemController.update);
+router.put("/:id", BillItemController.update);
 router.delete("/:id", BillItemController.delete);
 
 module.exports = router;
