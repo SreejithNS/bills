01122aa9eab1cf7eeec4fa3095ef6e6e00ee6bf0
SreejithNS <nssreejith229@gmail.com>
2020-12-05 19:53:52 +0530

    Update for 404 not Found

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -7,25 +7,26 @@ import AppBottomNavigation from '../components/AppBottomNavigation';
 import FullScreenLoading from '../components/FullScreenLoading';
 import AccountRoutes from './account.routes';
 import { paths } from './paths.enum';
-import NotFoundPage from '../pages/NotFoundPage';
 
-const HomePage = React.lazy(() => import('../pages/HomePage'));
-const BillsRoutes = React.lazy(() => import('./bills.routes'));
-const CustomersRoutes = React.lazy(() => import('./customers.routes'));
-const ItemsRoutes = React.lazy(() => import('./items.routes'));
-
-//import HomePage from '../pages/HomePage';
+// import NotFoundPage from '../pages/NotFoundPage';
+import HomePage from '../pages/HomePage';
 //import BillsRoutes from './bills.routes';
 //import CustomersRoutes from './customers.routes';
 //import ItemsRoutes from './items.routes';
+
+// const HomePage = React.lazy(() => import('../pages/HomePage'));
+const BillsRoutes = React.lazy(() => import('./bills.routes'));
+const NotFoundPage = React.lazy(() => import('../pages/NotFoundPage'));
+const CustomersRoutes = React.lazy(() => import('./customers.routes'));
+const ItemsRoutes = React.lazy(() => import('./items.routes'));
+
+
 
 export default function Routes() {
     return (
         <><Switch>
             <Route exact path={paths.home}>
-                <Suspense fallback={<FullScreenLoading />}>
-                    <HomePage />
-                </Suspense>
+                <HomePage />
             </Route>
             <Route path={paths.billsHome}>
                 <Suspense fallback={<FullScreenLoading />}>
@@ -48,7 +49,9 @@ export default function Routes() {
                 </Suspense>
             </Route>
             <Route>
-                <NotFoundPage />
+                <Suspense fallback={<FullScreenLoading />}>
+                    <NotFoundPage />
+                </Suspense>
             </Route>
         </Switch>
             <AppBottomNavigation /></>
