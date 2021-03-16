ea095a345cc48e9e76462803a726f2b6d868f779
SreejithNS <nssreejith229@gmail.com>
2021-03-16 10:33:54 +0530

    Added JSdoc types to helpers

--- server/helpers/privilegeEnum.js
+++ server/helpers/privilegeEnum.js
@@ -1,4 +1,22 @@
-module.exports = {
-    admin:1,
-    salesman:2
+/**
+ * Enum for User Types
+ * @readonly
+ * @enum {number}
+ */
+exports.privilegeEnum = {
+    root: 0,
+    admin: 1,
+    salesman: 2
 };
+
+/**
+ * Default Salesman Permissions
+ * @readonly
+ */
+exports.defaultSalesmanPermissions = [
+    "ALLOW_BILL_POST",
+    "ALLOW_BILL_GET",
+    "ALLOW_CUSTOMER_POST",
+    "ALLOW_CUSTOMER_GET",
+    "ALLOW_PRODUCT_GET"
+]
