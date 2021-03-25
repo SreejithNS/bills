bdee7921d1e4733e8acbdb72226af086d2a803e2
SreejithNS <nssreejith229@gmail.com>
2021-03-25 20:55:10 +0530

    Changed default salesman permisions

--- server/helpers/privilegeEnum.js
+++ server/helpers/privilegeEnum.js
@@ -16,7 +16,13 @@ exports.privilegeEnum = {
 exports.defaultSalesmanPermissions = [
     "ALLOW_BILL_POST",
     "ALLOW_BILL_GET",
+    "ALLOW_BILL_PUT",
+    "ALLOW_PRODUCTCATEGORY_GET",
     "ALLOW_CUSTOMER_POST",
     "ALLOW_CUSTOMER_GET",
-    "ALLOW_PRODUCT_GET"
+    "ALLOW_PRODUCT_GET",
+    "ALLOW_PAGE_ITEMS",
+    "ALLOW_PAGE_BILLS",
+    "ALLOW_PAGE_ACCOUNTS",
+    "ALLOW_PAGE_HOME",
 ]
