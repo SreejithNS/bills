700710d3aa687e65f6c37fc8280f8e197fec13bf
SreejithNS <nssreejith229@gmail.com>
2021-04-15 16:06:38 +0530

    Customer delete route added

--- server/routes/bill.js
+++ server/routes/bill.js
@@ -1,11 +1,12 @@
 var express = require("express");
-const { getAllBills, getBill, receivePayment, saveBill, toggleBillCredit } = require("../controllers/BillController");
+const { getAllBills, getBill, receivePayment, deleteBill, saveBill, toggleBillCredit } = require("../controllers/BillController");
 
 var router = express.Router();
 
 router.post("/", saveBill);
 router.get("/", getAllBills);
 router.get("/id/:billId", getBill);
+router.delete("/id/:billId", deleteBill);
 
 router.post("/:billId/payment", receivePayment);
 router.put("/:billId/credit", toggleBillCredit);
