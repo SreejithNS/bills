4f4debe89752174755f0ac999d77677cd480adb5
SreejithNS <nssreejith229@gmail.com>
2021-04-20 17:36:18 +0530

    Added icon to home page cards

--- client/src/components/HomeCard/index.tsx
+++ client/src/components/HomeCard/index.tsx
@@ -3,18 +3,42 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import { createStyles, makeStyles, Theme } from '@material-ui/core';
 
-export default function HomeCard(props: { classes?: any; onClick: any; title?: any; content?: any; }) {
+const useStyles = makeStyles((theme: Theme) => createStyles({
+    content: {
+        display: "flex",
+        flexFlow: "row nowrap",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        "&>*": {
+            display: "inline-block"
+        },
+        "&>*:first-child": {
+            marginRight: theme.spacing(2)
+        }
+    }
+}))
+
+export default function HomeCard(props: { classes?: any; onClick: any; title?: any; content?: any; icon?: any; }) {
+    const classes = useStyles();
     return (
         <Card className={props.classes || ""} style={{ height: "100%" }}>
             <CardActionArea onClick={props.onClick}>
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        {props.title || ""}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        {props.content || ""}
-                    </Typography>
+                <CardContent className={classes.content}>
+                    {props.icon &&
+                        <div>
+                            {props.icon}
+                        </div>
+                    }
+                    <div>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            {props.title || ""}
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            {props.content || ""}
+                        </Typography>
+                    </div>
                 </CardContent>
             </CardActionArea>
         </Card>
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Typography } from '@material-ui/core';
+import { Box, Grid, Typography } from '@material-ui/core';
 import HomeCard from '../components/HomeCard';
 import { useHistory } from 'react-router-dom';
 import { paths, billsPaths, itemPaths, customersPaths } from '../routes/paths.enum';
@@ -7,13 +7,19 @@ import PageContainer from '../components/PageContainer';
 import { version as appVersion } from '../../package.json';
 import { useHasPermission } from '../actions/auth.actions';
 import { UserPermissions } from '../reducers/auth.reducer';
+import { Receipt } from '@material-ui/icons';
+import PostAddIcon from '@material-ui/icons/PostAdd';
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import HelpIcon from '@material-ui/icons/Help';
 
 export default function HomePage() {
     const history = useHistory();
     const openLink = (link: string) => () => history.push(link);
 
     const itemsPageAccess = useHasPermission(UserPermissions.ALLOW_PAGE_ITEMS);
+    const createItemAccess = useHasPermission(UserPermissions.ALLOW_PRODUCT_POST);
     const billsPageAccess = useHasPermission(UserPermissions.ALLOW_PAGE_BILLS);
+    const createBillAccess = useHasPermission(UserPermissions.ALLOW_BILL_POST);
     const customersPageAccess = useHasPermission(UserPermissions.ALLOW_PAGE_CUSTOMERS);
 
     return (
@@ -30,9 +36,10 @@ export default function HomePage() {
                         Good Day!
                             </Typography>
                 </Grid>
-                {billsPageAccess
+                {billsPageAccess && createBillAccess
                     && <Grid item xs={12} sm={6} md={4}>
                         <HomeCard
+                            icon={<Receipt fontSize="large" />}
                             onClick={openLink(paths.billsHome + billsPaths.addBill)}
                             title="New Bill"
                             content="Add new bill from a customer."
@@ -40,9 +47,10 @@ export default function HomePage() {
                     </Grid>
                 }
 
-                {itemsPageAccess
+                {itemsPageAccess && createItemAccess
                     && <Grid item xs={12} sm={6} md={4}>
                         <HomeCard
+                            icon={<PostAddIcon fontSize="large" />}
                             onClick={openLink(paths.items + itemPaths.addItem)}
                             title="New Item"
                             content="Add new item to the inventory."
@@ -53,6 +61,7 @@ export default function HomePage() {
                 {customersPageAccess
                     && <Grid item xs={12} sm={6} md={4}>
                         <HomeCard
+                            icon={<PersonAddIcon fontSize="large" />}
                             onClick={openLink(paths.customer + customersPaths.createCustomer)}
                             title="New Customer"
                             content="Add new customer with details like phone number and place."
@@ -63,6 +72,7 @@ export default function HomePage() {
                 {billsPageAccess
                     && <Grid item xs={12} sm={6} md={4}>
                         <HomeCard
+                            icon={<Box fontSize="2.1875rem" width="2.1875rem" textAlign="center">₹</Box>}
                             onClick={openLink(paths.billsHome)}
                             title="Receive Payment"
                             content="Receive Payments for Credit Bills by searching them with customer name or serial number."
@@ -71,11 +81,11 @@ export default function HomePage() {
                 }
                 <Grid item xs={12} sm={6} md={4}>
                     <HomeCard
+                        icon={<HelpIcon fontSize="large" />}
                         onClick={() => console.log("Everything works fine.")}
                         content={"Bills WebApp: v" + appVersion}
                     />
                 </Grid>
-
             </Grid>
         </PageContainer>
     )
