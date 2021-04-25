d5006286df79602a9679c8d6af3aeac70f6ea279
SreejithNS <nssreejith229@gmail.com>
2021-04-25 13:25:44 +0530

    Added payment delete route

--- server/routes/bill.js
+++ server/routes/bill.js
@@ -1,5 +1,5 @@
 var express = require("express");
-const { getAllBills, getBill, receivePayment, deleteBill, saveBill, toggleBillCredit } = require("../controllers/BillController");
+const { getAllBills, getBill, receivePayment, deletePayment, deleteBill, saveBill, toggleBillCredit } = require("../controllers/BillController");
 
 var router = express.Router();
 
@@ -10,5 +10,5 @@ router.delete("/id/:billId", deleteBill);
 
 router.post("/:billId/payment", receivePayment);
 router.put("/:billId/credit", toggleBillCredit);
-
+router.delete("/:billId/payment/:paymentId", deletePayment);
 module.exports = router;
