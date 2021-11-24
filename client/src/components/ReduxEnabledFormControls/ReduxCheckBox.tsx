9ad30fce3b520172cd53e5b7e873ef567bc08310
SreejithNS <nssreejith229@gmail.com>
2021-11-25 00:38:23 +0530

    Added multiline and label

--- client/src/components/ReduxEnabledFormControls/ReduxCheckBox.tsx
+++ client/src/components/ReduxEnabledFormControls/ReduxCheckBox.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
+import { FormControlLabel } from '@material-ui/core';
 
-const ReduxCheckbox = ({ input }: any) => (
-    <Checkbox
-        checked={input.value ? true : false}
-        onChange={input.onChange}
-    />
-);
+const ReduxCheckbox = ({ input, label }: any) => {
+    return (
+        <FormControlLabel
+            control={
+                <Checkbox
+                    checked={input.value ? true : false}
+                    onChange={input.onChange}
+                />
+            }
+            label={label || ''}
+        />
+
+    )
+};
 
 export default ReduxCheckbox;
--- client/src/components/ReduxEnabledFormControls/ReduxTextField.tsx
+++ client/src/components/ReduxEnabledFormControls/ReduxTextField.tsx
@@ -2,19 +2,20 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 
 const ReduxTextField = (
-    { input, label, meta: { touched, error }, helperText, variant, type }: any
+    { input, multiline, label, meta: { touched, error }, helperText, variant, type }: any
 ) => (
-        <TextField
-            variant={variant || undefined}
-            onBlur={input.onBlur}
-            type={type || "text"}
-            error={touched && error}
-            label={label}
-            fullWidth
-            onChange={input.onChange}
-            value={input.value}
-            helperText={error || helperText || ""}
-        />
-    );
+    <TextField
+        variant={variant || undefined}
+        onBlur={input.onBlur}
+        type={type || "text"}
+        error={touched && error}
+        label={label}
+        fullWidth
+        multiline={!!(multiline ?? false)}
+        onChange={input.onChange}
+        value={input.value}
+        helperText={error || helperText || ""}
+    />
+);
 
 export default ReduxTextField;
