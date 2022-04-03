78940454e932b73e9dc3738c698105d3004800dc
SreejithNS <nssreejith229@gmail.com>
2022-04-03 12:31:25 +0530

    Rearranged the route order

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,15 +3,12 @@ var app = express();
 var loginRouter = require("../routes/login");
 var apiRouter = require("../routes/api");
 var apiResponse = require("../helpers/apiResponse");
+const path = require("path");
 
 const forest = require("forest-express-mongoose");
 var mongoose = require("mongoose");
 
-/* GET home page. */
-app.all("/", function (req, res) {
-	return apiResponse.successResponse(res, "Bills Server is up and running smoothly");
-});
-app.all("/check", function (req, res) {
+app.get("/check", function (req, res) {
 	return apiResponse.successResponse(res, "Bills Server is up and running smoothly");
 });
 app.use("/login", loginRouter);
@@ -30,10 +27,29 @@ forest.init({
 	connections: { default: mongoose.connection },
 }).then((FAMiddleware) => {
 	app.use(FAMiddleware);
-
-	app.use("/*", function (req, res) {
-		return apiResponse.notFoundResponse(res, "Page not found");
+	// app.use("/static", express.static(path.resolve(__dirname, "../../client", "build", "static")));
+	app.use("/", express.static(path.resolve(__dirname, "../../client", "build")));
+	// app.use("/**/*", function (req, res) {
+	// 	res.sendFile("index.html", path.join(__dirname, "../../client/build/"));
+	// });
+	app.get("/*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "../../client", "build", "index.html"));
 	});
+	// app.use("/*", express.static(path.resolve(__dirname, "../../client", "build")));
+
+
+	// app.use("/*", function (req, res) {
+	// 	return apiResponse.notFoundResponse(res, "Page not found");
+	// });
 });
+
+
+// /* GET home page. */
+// // app.all("/", function (req, res) {
+// // 	return apiResponse.successResponse(res, "Bills Server is up and running smoothly");
+// // });
+// app.get("/*", (req, res) => {
+// 	res.sendFile(path.resolve(__dirname, "../../client", "build", "index.html"));
+// });
 
 module.exports = app;
