3226d0cdeaa78b7f2c71e9d2251dc47a54a061d2
SreejithNS <nssreejith229@gmail.com>
2022-06-07 20:07:31 +0530

    Added few utlitity functions

--- server/helpers/privilegedAccess.js
+++ server/helpers/privilegedAccess.js
@@ -1,17 +1,54 @@
 const apiResponse = require("./apiResponse");
-const privilegeEnum = require("./privilegeEnum");
+const { privilegeEnum } = require("./privilegeEnum");
+const mongoose = require("mongoose");
+const { User } = require("../models/UserModel");
 
-exports.isPrivilegedAs = (checkFor) => (req, res, next) => {
-    if (req.user.type === checkFor) {
-        return next();
-    } else {
+/**
+ * @param {privilegeEnum|privilegeEnum[]} checkFor 
+ * @returns Middleware function
+ */
+exports.onlyFor = (checkFor) => (req, res, next) => {
+    if (Array.isArray(checkFor)) {
+        for (let i = 0; i < checkFor.length; i++) {
+            if (req.user.type === checkFor[i]) return next();
+        }
         return apiResponse.unauthorizedResponse(res, "You don't have the privilege to access this endpoint");
+    } else {
+        if (req.user.type === checkFor) {
+            return next();
+        } else {
+            return apiResponse.unauthorizedResponse(res, "You don't have the privilege to access this endpoint");
+        }
     }
 };
 
-module.isAdmin = (req, res, next) => {
-    if(req.user.type !== privilegeEnum.admin){
-       return apiResponse.unauthorizedResponse(res,"You are not authorised");
-    }
-    return next();
+module.onlyAdmins = (req, res, next) => {
+    return exports.onlyFor(privilegeEnum.admin)(req, res, next);
 };
+
+/**
+ * Recursively fetch the admin of the user.
+ * @param {string|mongoose.Types.ObjectId|User} identity - User's id or document
+ * @returns Admin of the user
+ */
+async function getAdmin(identity) {
+    if (typeof identity === "string" || mongoose.Types.ObjectId.isValid(identity)) {
+        const admin = await User.findById(identity);
+        if (!admin) throw new Error("Invalid User");
+
+        if (admin.type === privilegeEnum.admin) {
+            return admin;
+        } else {
+            return await getAdmin(admin.belongsTo);
+        }
+    } else if (identity instanceof User) {
+        if (identity.type === privilegeEnum.admin) {
+            return identity;
+        } else {
+            if (!identity.populated("belongsTo")) await identity.populate("belongsTo");
+            return await getAdmin(identity.belongsTo);
+        }
+    }
+}
+
+exports.getAdmin = getAdmin;
