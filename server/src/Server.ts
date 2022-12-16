7da85e1d9f98023dcd29d47f7cfbd9ebbe4b225a
SreejithNS <nssreejith229@gmail.com>
2022-12-16 22:23:38 +0530

    Added: Basic Server codes

---
+++ server/src/Server.ts
@@ -0,0 +1,55 @@
+import { Application, RequestHandler, Router } from "express";
+import { Server as ServerType } from "http";
+import mongoose from "mongoose";
+import Controller from "./controllers";
+
+export default class Server {
+  private app: Application;
+
+  private apiBasePath = /^\/api(?=\/|$)/;
+
+  private readonly port: number;
+
+  public static isDev = process.env.NODE_ENV !== "production";
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // loadMiddleware(globalMiddleware: RequestHandler[]) {}
+
+  constructor(app: Application, port: number) {
+    this.app = app;
+    this.port = port;
+  }
+
+  public run(): ServerType {
+    return this.app.listen(this.port, () => {
+      console.log(`Up and running on port ${this.port}`);
+    });
+  }
+
+  public loadGlobalMiddleware(middleware: Array<RequestHandler>): void {
+    // global stuff like cors, body-parser, etc
+    middleware.forEach((mw) => {
+      this.app.use(mw);
+    });
+  }
+
+  public loadControllers(controllers: Array<Controller>): void {
+    const router = Router();
+    controllers.forEach((controller) => {
+      // use setRoutes method that maps routes and returns Router object
+      router.use(controller.path, controller.setRoutes());
+    });
+
+    this.app.use(this.apiBasePath, router); // Matches only /api
+  }
+
+  public async initDatabase(): Promise<typeof mongoose> {
+    // connect to mongoose connection and return promise
+    const user = encodeURIComponent(process.env.MONGO_USER ?? "");
+    const passwd = encodeURIComponent(process.env.MONGO_PWD ?? "");
+    const cluster = process.env.MONGO_CLUSTER;
+    const MONGO_URL = `mongodb+srv://${user}:${passwd}@${cluster}/Main?retryWrites=true&w=majority`;
+
+    return mongoose.connect(MONGO_URL ?? "", {});
+  }
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,29 @@
-const string = "Hello World";
-console.log(string);
+import * as express from "express";
+import { json, RequestHandler, urlencoded } from "express";
+import * as dotenv from "dotenv";
+import Controller from "./controllers/index";
+import Server from "./Server";
+
+dotenv.config({ path: "./.env.local" });
+const app = express();
+const port = parseInt(process.env.PORT ?? "", 10) || 3000;
+
+const server = new Server(app, port);
+
+const controllers: Array<Controller> = [];
+
+const globalMiddleware: Array<RequestHandler> = [
+  urlencoded({ extended: false }),
+  json(),
+  // ...
+];
+
+Promise.resolve()
+  .then(() => server.initDatabase())
+  .then(async () => {
+    server.loadGlobalMiddleware(globalMiddleware);
+    server.loadControllers(controllers);
+    server.run();
+  });
+
+export {};
