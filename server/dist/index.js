4b40b7c192986e9e2bafb8948d32e1fc5e4c8498
SreejithNS <nssreejith229@gmail.com>
2022-12-16 20:05:39 +0530

    Added husky

--- server/dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var string = "Hello World";
-console.log(string);
-//# sourceMappingURL=index.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,2 @@
 const string = "Hello World";
-console.log(string);
+console.log(string)
