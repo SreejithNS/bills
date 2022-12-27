151354ab302362bcfb57724d22adab143746f1e1
SreejithNS <nssreejith229@gmail.com>
2022-12-27 11:34:29 +0530

    Modified base controller

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -1,4 +1,13 @@
 import { Response, Request, NextFunction, Router } from "express";
+import { ValidationChain, validationResult } from "express-validator";
+import mongoose from "mongoose";
+import { MiddlewareFunction } from "../utils/Types";
+import {
+	ErrorResponse,
+	InternalServerError,
+	MongooseValidationError,
+	ValidationError,
+} from "../utils/Errors";
 
 // HTTP methods
 export enum Methods {
@@ -10,11 +19,12 @@ export enum Methods {
 }
 
 // Route interface for each route in `routes` field of `Controller` class.
-interface IRoute {
+export interface IRoute {
 	path: string;
 	method: Methods;
 	handler: (req: Request, res: Response, next: NextFunction) => void | Promise<void>;
-	localMiddleware: ((req: Request, res: Response, next: NextFunction) => void)[];
+	localMiddleware: ((req: Request, res: Response, next: NextFunction) => void | Promise<void>)[];
+	validation?: ValidationChain[];
 }
 
 export default abstract class Controller {
@@ -24,31 +34,50 @@ export default abstract class Controller {
 	// The path on which this.routes will be mapped
 	public abstract path: string;
 
+	// Array of Middleware functions for this Controller
+	public abstract middlewares: Record<
+		string,
+		((...arg0: unknown[]) => Promise<MiddlewareFunction>) | MiddlewareFunction
+	>;
+
 	// Array of objects which implement IRoutes interface
 	protected abstract readonly routes: Array<IRoute>;
 
-	public setRoutes = (): Router => {
+	public setRoutes(): Router {
 		// Set HTTP method, middleware, and handler for each route
 		// Returns Router object, which we will use in Server class
 		for (const route of this.routes) {
 			for (const mw of route.localMiddleware) {
 				this.router.use(route.path, mw);
 			}
+
+			const routeArguments: [
+				string,
+				...IRoute["localMiddleware"],
+				ReturnType<typeof this.validate> | undefined,
+				IRoute["handler"]
+			] = [
+				route.path,
+				...route.localMiddleware,
+				this.validate(route.validation),
+				route.handler.bind(this),
+			];
+
 			switch (route.method) {
 				case "GET":
-					this.router.get(route.path, ...route.localMiddleware, route.handler);
+					this.router.get(...routeArguments);
 					break;
 				case "POST":
-					this.router.post(route.path, ...route.localMiddleware, route.handler);
+					this.router.post(...routeArguments);
 					break;
 				case "PUT":
-					this.router.put(route.path, ...route.localMiddleware, route.handler);
+					this.router.put(...routeArguments);
 					break;
 				case "DELETE":
-					this.router.delete(route.path, ...route.localMiddleware, route.handler);
+					this.router.delete(...routeArguments);
 					break;
 				case "ALL":
-					this.router.all(route.path, ...route.localMiddleware, route.handler);
+					this.router.all(...routeArguments);
 					break;
 				default:
 				// Throw exception
@@ -56,5 +85,31 @@ export default abstract class Controller {
 		}
 		// Return router instance (will be usable in Server class)
 		return this.router;
-	};
+	}
+
+	protected validate(validations?: ValidationChain[]) {
+		return async (req: Request, res: Response, next: NextFunction) => {
+			if (!validations || validations.length === 0) return next();
+
+			await Promise.all(validations.map((validation) => validation.run(req)));
+
+			const errors = validationResult(req);
+			if (errors.isEmpty()) {
+				return next();
+			}
+
+			return new ValidationError("Validation failed", errors).handleResponse(res);
+		};
+	}
+
+	protected handleError(error: Error, res: Response) {
+		if (error instanceof ErrorResponse) {
+			return error.handleResponse(res);
+		}
+		if (error instanceof mongoose.Error.ValidationError) {
+			return new MongooseValidationError(error).handleResponse(res);
+		}
+
+		return new InternalServerError(error.message).handleResponse(res);
+	}
 }
